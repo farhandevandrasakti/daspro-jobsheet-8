f420b31dbe2470fef1444cdbfffb21c61581a05c
farhandevandrasakti <farhandevandrasakti07@gmail.com>
2025-10-30 11:29:38 +0700

    NilaiKelompok12

---
+++ NilaiKelompok12.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NilaiKelompok12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalNilai, nilai;
+        double rataNilai;
+        int i = 1;
+
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0;
+            
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(" Nilai dari Kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+            
+            rataNilai = totalNilai / 5.0;
+            System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
+            i++;
+        }
+    }
+}
